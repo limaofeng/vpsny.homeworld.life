87740f3d6b3f049deb4313bcbb60bdf3cb44e6af
李茂峰 <limaofeng@msn.com>
2018-10-24 20:28:21 +0800

    添加隐私声明页面

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,15 @@ const router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '' });
+  res.render('index', { title: 'VPS Manager' });
+});
+
+router.get('/support', function(req, res) {
+  res.render('support', { title: 'Privacy Statement' });
+});
+
+router.get('/privacy', function(req, res) {
+  res.render('privacy', { title: 'Privacy Statement' });
 });
 
 export default router;
